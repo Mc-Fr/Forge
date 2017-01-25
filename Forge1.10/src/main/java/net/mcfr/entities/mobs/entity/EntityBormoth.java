a147b9c585322b52e8d3e02618cbff5b675fd1b6
Thomas <Thomas@DESKTOP-LHB2BGT>
2017-01-25 01:21:35 +0100

    feat: texture bormoth, amélioration animation

--- Forge1.10/src/main/java/net/mcfr/entities/mobs/entity/EntityBormoth.java
+++ Forge1.10/src/main/java/net/mcfr/entities/mobs/entity/EntityBormoth.java
@@ -16,6 +16,7 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAIPanic;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
@@ -54,6 +55,7 @@ public class EntityBormoth extends EntityBurrowed {
 
   protected void initEntityAI() {
     this.tasks.addTask(0, new EntityAISwimming(this));
+    this.tasks.addTask(1, new EntityAIPanic(this, 1.4D));
     this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
     this.tasks.addTask(4, new EntityAIFollowParent(this, 1.1D));
     this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
