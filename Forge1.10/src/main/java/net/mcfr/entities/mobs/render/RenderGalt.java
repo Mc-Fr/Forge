14801cec516eb8c2e06dd037560a1efff585c293
Thomas <Thomas@DESKTOP-LHB2BGT>
2017-01-25 23:16:53 +0100

    feat: multi-textures galt

--- Forge1.10/src/main/java/net/mcfr/entities/mobs/render/RenderGalt.java
+++ Forge1.10/src/main/java/net/mcfr/entities/mobs/render/RenderGalt.java
@@ -8,7 +8,8 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
 
 public class RenderGalt extends RenderLiving<EntityGalt> {
-  private static final ResourceLocation GALT_TEXTURES = new ResourceLocation(Constants.MOD_ID, "textures/entity/galt.png");
+  private static final ResourceLocation GALT_TEXTURES_1 = new ResourceLocation(Constants.MOD_ID, "textures/entity/galt1.png");
+  private static final ResourceLocation GALT_TEXTURES_2 = new ResourceLocation(Constants.MOD_ID, "textures/entity/galt2.png");
 
   public RenderGalt(RenderManager renderManagerIn, ModelGalt modelbaseIn, float shadowSizeIn) {
     super(renderManagerIn, modelbaseIn, shadowSizeIn);
@@ -19,7 +20,11 @@ public class RenderGalt extends RenderLiving<EntityGalt> {
    * unless you call Render.bindEntityTexture.
    */
   protected ResourceLocation getEntityTexture(EntityGalt entity) {
-    return GALT_TEXTURES;
+    if (entity.getFurType() == 1) {
+      return GALT_TEXTURES_1;
+    } else {
+      return GALT_TEXTURES_2;
+    }
   }
 
   /**
