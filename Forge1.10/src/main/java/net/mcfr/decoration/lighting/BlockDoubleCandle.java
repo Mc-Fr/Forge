d2e0830fb04d7aa8de49f4fec2b59b72e7ad2f00
Darmo <Darmo@PC>
2017-01-31 15:57:17 +0100

    feat : double bougie (pas de flamme pour l'instant)

---
+++ Forge1.10/src/main/java/net/mcfr/decoration/lighting/BlockDoubleCandle.java
@@ -0,0 +1,20 @@
+package net.mcfr.decoration.lighting;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+public class BlockDoubleCandle extends BlockCandle {
+  // TEMP
+  private static final AxisAlignedBB AABB = FULL_BLOCK_AABB;
+
+  public BlockDoubleCandle() {
+    super("double", 0.5f);
+  }
+
+  @Override
+  public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    return AABB;
+  }
+}
