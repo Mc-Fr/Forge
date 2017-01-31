eefe153f7eafef38fc219c50265ac8a0ba03e043
Darmo <Darmo@PC>
2017-01-31 15:52:09 +0100

    feat : flammes des bougies

--- Forge1.10/src/main/java/net/mcfr/decoration/lighting/BlockSimpleCandle.java
+++ Forge1.10/src/main/java/net/mcfr/decoration/lighting/BlockSimpleCandle.java
@@ -1,32 +1,20 @@
 package net.mcfr.decoration.lighting;
 
-import java.util.Random;
-
+import net.mcfr.utils.math.Point3d;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public class BlockSimpleCandle extends BlockCandle {
+  private static final AxisAlignedBB AABB = new AxisAlignedBB(0.46875, 0, 0.46875, 0.59375, 0.5, 0.59375);
+
   public BlockSimpleCandle() {
-    super("simple", 0.5F);
+    super("simple", 0.5f, new Point3d(0.53125, 0.625, 0.53125));
   }
 
   @Override
   public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-    return new AxisAlignedBB(0.436F, 0, 0.436F, 0.564F, 0.5F, 0.564F);
-  }
-
-  @Override
-  public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-    return Item.getItemFromBlock(this);
-  }
-
-  @Override
-  public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
-    return new ItemStack(this);
+    return AABB;
   }
 }
