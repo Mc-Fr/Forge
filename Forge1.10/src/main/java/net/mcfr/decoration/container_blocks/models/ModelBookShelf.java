19ec70a4c9f8003f8700e648b7068d4c53917551
Darmo <Darmo@PC>
2017-02-07 15:29:34 +0100

    doc : package net.mcfr.decoration.container_blocks.models

--- Forge1.10/src/main/java/net/mcfr/decoration/container_blocks/models/ModelBookShelf.java
+++ Forge1.10/src/main/java/net/mcfr/decoration/container_blocks/models/ModelBookShelf.java
@@ -3,6 +3,11 @@ package net.mcfr.decoration.container_blocks.models;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 
+/**
+ * Modèle de la bibliothèque.
+ *
+ * @author Mc-Fr
+ */
 public class ModelBookShelf extends ModelBase {
   private ModelRenderer block;
 
@@ -11,6 +16,11 @@ public class ModelBookShelf extends ModelBase {
     this.block.addBox(0, 0, 0, 32, 32, 32);
   }
 
+  /**
+   * Affiche le modèle.
+   * 
+   * @param scale l'échelle
+   */
   public void renderModel(float scale) {
     this.block.render(scale);
   }
