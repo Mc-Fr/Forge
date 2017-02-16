d6f7d59c82051552a5b23316035e066389384faa
Darmo <Darmo@PC>
2017-02-16 23:06:38 +0100

    refactor : paquet destruction bulle de tchat

--- Forge1.10/src/main/java/net/mcfr/guis/GuiMcfrChat.java
+++ Forge1.10/src/main/java/net/mcfr/guis/GuiMcfrChat.java
@@ -3,7 +3,6 @@ package net.mcfr.guis;
 import net.mcfr.network.CreateChatBubbleMessage;
 import net.mcfr.network.DestroyChatBubbleMessage;
 import net.mcfr.network.McfrNetworkWrapper;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -19,6 +18,6 @@ public class GuiMcfrChat extends GuiChat {
   @Override
   public void onGuiClosed() {
     super.onGuiClosed();
-    McfrNetworkWrapper.getInstance().sendToServer(new DestroyChatBubbleMessage(Minecraft.getMinecraft().thePlayer.getUniqueID()));
+    McfrNetworkWrapper.getInstance().sendToServer(new DestroyChatBubbleMessage());
   }
 }
--- Forge1.10/src/main/java/net/mcfr/network/DestroyChatBubbleMessage.java
+++ Forge1.10/src/main/java/net/mcfr/network/DestroyChatBubbleMessage.java
@@ -1,55 +1,32 @@
 package net.mcfr.network;
 
-import java.util.UUID;
-
 import io.netty.buffer.ByteBuf;
 import net.mcfr.guis.chat_bubble.ChatBubble;
 import net.minecraft.entity.Entity;
-import net.minecraft.world.World;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class DestroyChatBubbleMessage implements IMessage {
-  private UUID playerUuid;
-
-  // Requis par Forge.
-  public DestroyChatBubbleMessage() {
-    this(null);
-  }
-
-  public DestroyChatBubbleMessage(UUID playerUuid) {
-    this.playerUuid = playerUuid;
-  }
+  @Override
+  public void toBytes(ByteBuf buf) {}
 
   @Override
-  public void toBytes(ByteBuf buf) {
-    buf.writeLong(getPlayerUuid().getLeastSignificantBits());
-    buf.writeLong(getPlayerUuid().getMostSignificantBits());
-  }
-
-  @Override
-  public void fromBytes(ByteBuf buf) {
-    long lsb = buf.readLong();
-    long msb = buf.readLong();
-    this.playerUuid = new UUID(msb, lsb);
-  }
-
-  public UUID getPlayerUuid() {
-    return this.playerUuid;
-  }
+  public void fromBytes(ByteBuf buf) {}
 
   public static class ServerHandler implements IMessageHandler<DestroyChatBubbleMessage, IMessage> {
     @Override
     public IMessage onMessage(final DestroyChatBubbleMessage message, MessageContext ctx) {
-      ctx.getServerHandler().playerEntity.getServerWorld().addScheduledTask(() -> {
-        World world = ctx.getServerHandler().playerEntity.worldObj;
-        int id = ChatBubble.PLAYER_BUBBLE.get(message.getPlayerUuid());
-        Entity e = world.getEntityByID(id);
+      EntityPlayerMP player = ctx.getServerHandler().playerEntity;
+
+      player.getServerWorld().addScheduledTask(() -> {
+        int id = ChatBubble.PLAYER_BUBBLE.get(player.getUniqueID());
+        Entity e = player.worldObj.getEntityByID(id);
 
         if (e != null) {
           e.setDead();
-          ChatBubble.PLAYER_BUBBLE.remove(message.getPlayerUuid());
+          ChatBubble.PLAYER_BUBBLE.remove(player.getUniqueID());
         }
       });
 
