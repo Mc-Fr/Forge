02b16061e1a1b702090e8e19f9a49e0df2416ff8
Darmo <Darmo@PC>
2017-02-21 21:01:41 +0100

    fix : bulle qui ne disparaissait pas

--- Forge1.10/src/main/java/net/mcfr/network/DestroyChatBubbleMessage.java
+++ Forge1.10/src/main/java/net/mcfr/network/DestroyChatBubbleMessage.java
@@ -25,7 +25,7 @@ public class DestroyChatBubbleMessage implements IMessage {
         Entity e = player.worldObj.getEntityByID(id);
 
         if (e != null) {
-          // e.setDead(); // DEBUG
+          e.setDead();
           ChatBubble.PLAYER_BUBBLE.remove(player.getUniqueID());
         }
       });
