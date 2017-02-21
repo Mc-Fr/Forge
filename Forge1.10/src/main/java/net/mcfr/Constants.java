30d5b250ddd906fb1dcf0db5584f038de5588619
Darmo <Darmo@PC>
2017-02-21 14:26:40 +0100

    feat : changement version

--- Forge1.10/src/main/java/net/mcfr/Constants.java
+++ Forge1.10/src/main/java/net/mcfr/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
   /** Nom du mod */
   public static final String MOD_NAME = "Mc-Fr";
   /** Version du mod */
-  public static final String MOD_VERSION = "1.1.1";
+  public static final String MOD_VERSION = "1.1.2";
 
   private Constants() {}
 }
