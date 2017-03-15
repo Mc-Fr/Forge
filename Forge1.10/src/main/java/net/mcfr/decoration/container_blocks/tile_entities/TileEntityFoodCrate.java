a2b763032e2d30272a3d64b040cc8f994741c87f
Thomas <Thomas@DESKTOP-LHB2BGT>
2017-03-15 23:27:05 +0100

    fix: fût de nourriture corrigé

--- Forge1.10/src/main/java/net/mcfr/decoration/container_blocks/tile_entities/TileEntityFoodCrate.java
+++ Forge1.10/src/main/java/net/mcfr/decoration/container_blocks/tile_entities/TileEntityFoodCrate.java
@@ -1,15 +1,12 @@
 package net.mcfr.decoration.container_blocks.tile_entities;
 
-import net.mcfr.decoration.container_blocks.BlockFoodCrate;
-import net.mcfr.decoration.container_blocks.guis.ContainerRestricted;
-
 /**
  * Tile entity du fût de nourriture.
  *
  * @author Mc-Fr
  */
-public class TileEntityFoodCrate extends TileEntityRestricted {
+public class TileEntityFoodCrate extends TileEntityLarge {
   public TileEntityFoodCrate() {
-    super("food_crate", ContainerRestricted.SIZE, 64, true, BlockFoodCrate.class, ContainerRestricted.class);
+    super("food_crate");
   }
 }
